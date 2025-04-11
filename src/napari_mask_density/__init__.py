a73cfb5af282a5b47dbaecdcc6d820720b95ecdc
Da Kuang <kuangda@seas.upenn.edu>
2025-04-11 10:01:53 -0700

    added core features

--- src/napari_mask_density/__init__.py
+++ src/napari_mask_density/__init__.py
@@ -3,23 +3,6 @@ try:
 except ImportError:
     __version__ = "unknown"
 
-from ._reader import napari_get_reader
-from ._sample_data import make_sample_data
-from ._widget import (
-    ExampleQWidget,
-    ImageThreshold,
-    threshold_autogenerate_widget,
-    threshold_magic_widget,
-)
-from ._writer import write_multiple, write_single_image
+from ._widget import MaskAnalyzer
 
-__all__ = (
-    "napari_get_reader",
-    "write_single_image",
-    "write_multiple",
-    "make_sample_data",
-    "ExampleQWidget",
-    "ImageThreshold",
-    "threshold_autogenerate_widget",
-    "threshold_magic_widget",
-)
+__all__ = ("__version__", "MaskAnalyzer")
